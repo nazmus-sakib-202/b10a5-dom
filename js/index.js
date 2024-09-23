eeed6fceee5d53ffe8eb2aa1b38b33c1a38a80bd
nazmus-sakib-202 <105826245+nazmus-sakib-202@users.noreply.github.com>
2024-09-24 04:09:48 +0600

    navbar backdrop blur completed

--- js/index.js
+++ js/index.js
@@ -7,3 +7,10 @@ function navigateToBlog(){
 function navigateToHome(){
     window.location.href = "index.html";
 }
+
+const buttonNoakhali = document.getElementById("nokhali_modal_5");
+buttonNoakhali.addEventListener("click", function(){
+    const noakhaliAmount = parseFloat(document.getElementById("nokhali-amount").value);
+    console.log(noakhaliAmount);
+
+})
